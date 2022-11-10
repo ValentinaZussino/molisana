4b4f9b3bfc8947aa66705f39363d40c843c251cc
ValentinaZussino <vzussino@protonmail.com>
2022-11-10 12:50:11 +0100

    inizio impostaz footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,14 @@
 <template>
     <footer>
-        Sono il Footer
+        <div class="cols container">
+            <a href="#" class="logo">
+                <img src="../assets/img/la-molisana-logo.png" alt="La Molisana">
+            </a>
+            <div class="footer-links">
+            </div>
+            <div class="footer-links">
+            </div>
+        </div>
     </footer>
 </template>
 
@@ -13,5 +21,18 @@
 <style lang="scss" scoped>
 @use '../assets/styles/partials/mixin' as *;
 @use '../assets/styles/partials/variables' as *;
+
+footer {
+    padding-top: 3rem;
+    .cols {
+        display: flex;
+        margin-bottom: 3rem;
+
+        .logo, footer-links {
+            flex-basis: calc(100% / 3);
+            padding: 0 1rem;
+        }
+    }
+}
 
 </style>
